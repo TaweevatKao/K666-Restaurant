51efd60f552e1e1795dd125fa3c3482a6b91d43a
TaweevatKao <kamiakuma2013@gmail.com>
2016-03-19 11:46:41 +0700

    add value  finish 11.45 n

--- app/src/main/java/rru/kao/taweevat/k666restaurant/MainActivity.java
+++ app/src/main/java/rru/kao/taweevat/k666restaurant/MainActivity.java
@@ -17,8 +17,18 @@ public class MainActivity extends AppCompatActivity {
         //Request SQLite
         myManage = new MyManage(this);
 
+        //test add value
+        testAddValue();
+
+
 
     }// main method
+
+    private void testAddValue() {
+        myManage.addValue(1, "user", "pass", "name");
+        myManage.addValue(2, "food", "price", "source");
+
+    }
 
 
 } //main class
--- app/src/main/java/rru/kao/taweevat/k666restaurant/MyManage.java
+++ app/src/main/java/rru/kao/taweevat/k666restaurant/MyManage.java
@@ -1,5 +1,6 @@
 package rru.kao.taweevat.k666restaurant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -8,9 +9,20 @@ import android.database.sqlite.SQLiteDatabase;
  */
 public class MyManage {
     //Eplicit
-
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "id";
+    public static final String column_User = "User";
+    public static final String column_Password = "Password";
+    public static final String column_Name = "Name";
+
+    public static final String food_table = "foodTABLE";
+    public static final String column_Food = "Food";
+    public static final String column_Price = "Price";
+    public static final String column_Source = "Source";
+
 
     public MyManage(Context context) {
 
@@ -20,5 +32,39 @@ public class MyManage {
         readSqLiteDatabase = myOpenHelper.getReadableDatabase();
 
     }//Constructer
+
+
+    public long addValue(int intTABLE,
+                         String strColumn2,
+                         String strColumn3,
+                         String strColumn4) {
+
+        ContentValues contentValues = new ContentValues();
+        long myLong = 0;
+
+        switch (intTABLE) {
+            case 1:
+                //for userTABLE
+                contentValues.put(column_User, strColumn2);
+                contentValues.put(column_Password, strColumn3);
+                contentValues.put(column_Name, strColumn4);
+
+                myLong = writeSqLiteDatabase.insert(user_table, null, contentValues);
+
+                break;
+            case 2:
+                //for foodTABLE
+                contentValues.put(column_Food, strColumn2);
+                contentValues.put(column_Price, strColumn3);
+                contentValues.put(column_Source, strColumn4);
+
+                myLong = writeSqLiteDatabase.insert(food_table, null, contentValues);
+
+                break;
+        }
+
+        return 0;
+    }
+
 
 } //main class
