4d014c58fe2568e9bc9af56de597e78d46ff3948
TaweevatKao <kamiakuma2013@gmail.com>
2016-03-19 14:13:13 +0700

    Syn JSON  finish 11.55 n

--- app/src/main/java/rru/kao/taweevat/k666restaurant/MainActivity.java
+++ app/src/main/java/rru/kao/taweevat/k666restaurant/MainActivity.java
@@ -1,8 +1,22 @@
 package rru.kao.taweevat.k666restaurant;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,10 +38,107 @@ public class MainActivity extends AppCompatActivity {
         //delete all sqlite
         deleteAllSQlite();
 
+        //Syn Jason to sqlite
+        sysJSONtoSQLite();
+
 
 
 
     }// main method
+
+    private void sysJSONtoSQLite() {
+
+        //Connect http://
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        int intTable = 0;
+        while (intTable <= 1) {
+
+            //1 Create InputStream
+            InputStream inputStream = null;
+            String[] urlStrings = {"http://swiftcodingthai.com/19Mar/php_get_user_kao.php",
+                    "http://swiftcodingthai.com/19Mar/php_get_food_kao.php"};
+
+            try {
+
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(urlStrings[intTable]);
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                inputStream = httpEntity.getContent();
+
+            } catch (Exception e) {
+                Log.d("Rest", "InputStream ==>" + e.toString());
+            }
+
+            //2 Create JSON String
+            String strJSON = null;
+
+            try {
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                StringBuilder stringBuilder = new StringBuilder();
+                String strLine = null;
+
+                while ((strLine = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(strLine);
+                }
+
+                inputStream.close();
+                strJSON = stringBuilder.toString();
+
+
+            } catch (Exception e) {
+                Log.d("Rest", "JSON String ==>" + e.toString());
+            }
+
+
+            //3 Update to SQLite
+            try {
+
+                JSONArray jsonArray = new JSONArray(strJSON);
+                for (int i = 0; i < jsonArray.length(); i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+
+                    switch (intTable) {
+                        case 0:
+
+                            String strUser = jsonObject.getString(MyManage.column_User);
+                            String strPassword = jsonObject.getString(MyManage.column_Password);
+                            String strName = jsonObject.getString(MyManage.column_Name);
+
+                            myManage.addValue(1, strUser, strPassword, strName);
+
+                            break;
+                        case 1:
+
+                            String strFood = jsonObject.getString(MyManage.column_Food);
+                            String strPrice = jsonObject.getString(MyManage.column_Price);
+                            String strSource = jsonObject.getString(MyManage.column_Source);
+
+                            myManage.addValue(2, strFood, strPrice, strSource);
+
+                            break;
+
+                    }
+
+                } //for
+
+
+            } catch (Exception e) {
+                Log.d("Rest", "Update SQLite ==>" + e.toString());
+            }
+
+
+            intTable += 1;
+        }//while
+
+
+    } //Syn Jason to sqlite
 
     private void deleteAllSQlite() {
 
